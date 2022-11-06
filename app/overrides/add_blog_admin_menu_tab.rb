51d2002ca413ea4535bd645699b1a2f05a8097cd
vansy <vansy61@gmail.com>
2022-11-06 09:20:52 +0700

    update

--- app/overrides/add_blog_admin_menu_tab.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Deface::Override.new(
-  virtual_path: "spree/admin/shared/_main_menu",
-  name: "blogs_admin_sidebar_menu",
-  insert_before: "ul#sidebarConfiguration",
-  partial: "spree/admin/shared/blogs_sidebar_menu",
-  disabled: false
-)
